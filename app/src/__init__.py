c98b8dc312888ee231984f925e1c28102325d405
Eric Ford <ericford@mac.com>
2021-01-07 19:37:55 -0500

    Flask api is done for cities and zips. Now do merchants. Still need darn tests for merchants on all classes.

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -30,57 +30,65 @@ def create_app(database='jigsaw_project_test', testing = TESTING, debug = DEBUGG
     def root_url():
         return 'Welcome to my api'
 
-    @app.route('/city/<name>')
-    def city(name):
+    @app.route('/city/<city_name>')
+    def city(city_name):
+        """Return complete record for city with name == city_name."""
         conn = db.get_db()
         cursor = conn.cursor()
-        city = db.find_by_name(models.City, name, cursor)
-        return json.dumps(city.__dict__['name'], default=str)
+        city = db.find_by_name(models.City, city_name, cursor)
+        return json.dumps(city.__dict__, default=str)
 
     @app.route('/cities')
     def cities():
+        """Return complete records for all cities in DB."""
         conn = db.get_db()
         cursor = conn.cursor()
         cities = db.find_all(models.City, cursor)
-        city_names = [city.__dict__['name'] for city in cities]
+        city_names = [city.__dict__ for city in cities]
         return json.dumps(city_names, default=str)
 
-    @app.route('/cities/search')
-    def search_cities():
+    # @app.route('/cities/search')
+    # def search_cities():
+    #     conn = db.get_db()
+    #     cursor = conn.cursor()
+
+    #     params = dict(request.args)
+    #     cities = models.city.search(params, cursor)
+    #     city_dicts = [city.to_json(cursor) for city in cities]
+    #     return json.dumps(city_dicts, default = str)
+
+    @app.route('/zips_for_city/<city_name>')
+    def zips_for_city(city_name):
+        """
+        For a city given by city_name, return complete record for all 
+        zipcodes in that city.
+        """
         conn = db.get_db()
         cursor = conn.cursor()
-
-        params = dict(request.args)
-        cities = models.city.search(params, cursor)
-        city_dicts = [city.to_json(cursor) for city in cities]
-        return json.dumps(city_dicts, default = str)
-
-    @app.route('/city_zips/<name>')
-    def city_zips(name):
-        conn = db.get_db()
-        cursor = conn.cursor()
-        zipcodes = find_by_name(City, name, cursor).zipcodes(cursor)
+        zipcodes = find_by_name(City, city_name, cursor).zipcodes(cursor)
         print(zipcodes)
-        zipcode_names = [zipcode.__dict__['name'] for zipcode in zipcodes]
+        zipcode_names = [zipcode.__dict__ for zipcode in zipcodes]
         return json.dumps(zipcode_names, default = str)
 
     @app.route('/zipcodes')
     def zipcodes():
+        """Return all zipcodes in DB."""
         conn = db.get_db()
         cursor = conn.cursor()
 
         zipcodes = db.find_all(models.Zipcode, cursor)
-        zipcode_dicts = [zipcode.__dict__['name'] for zipcode in zipcodes]
+        zipcode_dicts = [zipcode.__dict__ for zipcode in zipcodes]
         return json.dumps(zipcode_dicts, default=str)
 
-    # @app.route('/cities_by_zip/<id>')
-    # def cities():
-    #     conn = db.get_db()
-    #     cursor = conn.cursor()
+    @app.route('/cities_for_zip/<zipcode>')
+    def cities_for_zip(zipcode):
+        """ For a zip with name zipcode, return all cities in that zipcode."""
+        conn = db.get_db()
+        cursor = conn.cursor()
 
-    #     cities = db.find_all(models.City, cursor)
-    #     city_dicts = [city.__dict__['name'] for city in cities]
-    #     return json.dumps(city_dicts, default=str)
+        cities = db.find_by_name(Zipcode, zipcode, cursor).cities(cursor)
+        city_dicts = [city.__dict__ for city in cities]
+        return json.dumps(city_dicts, default=str)
 
     return app
 
